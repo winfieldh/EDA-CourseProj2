6d3c9f4ef9fd226670775b6fcb6e1e4f986d3359
Winfield Henry <winfieldh@gmail.com>
2017-07-25 11:09:32 -0400

    update

--- index.R
+++ index.R
@@ -9,7 +9,7 @@ classcode <- readRDS("Source_Classification_Code.rds")
 #1 make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008
 
 TotalEmissions <- group_by(pmdata, year) %>% summarise(Emissions=sum(Emissions))
-
+plot(TotalEmissions$year,TotalEmissions$Emissions, type="l", col="red",xlab="Year", ylab="Emissions", main = "Total Emissions")
 
 #2 Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot answering this question.
 
